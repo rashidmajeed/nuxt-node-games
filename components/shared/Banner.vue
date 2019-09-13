f5cfc6d946a7b3fa59aa926cba2daa7e5257f928
rashidmajeed <rashid.caliber@gmail.com>
2019-09-13 18:04:54 +0200

    Added shared components for the background banner, gamecard and article card

---
+++ components/shared/Banner.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="hero is-black is-medium">
+      <div class="hero-body">
+        <div
+          class="hero-img"
+          :style="{ background : `url(https://images3.alphacoders.com/100/thumb-1920-1007538.jpg) no-repeat center center`}"
+        ></div>
+        <div class="container">
+          <h1 class="title">Top Ranked Video Games</h1>
+          <h2 class="subtitle">Thrill, Adventure, Suspense and FUN :)</h2>
+          <a target="_" :href="'#'" class="button is-danger">Learn More!</a>
+        </div>
+      </div>
+    </section>
+</template>
+<style lang="scss">
+.hero-body {
+  position: relative;
+}
+.hero-img {
+  opacity: 0.8;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+.user-avatar {
+  display: inline-block;
+}
+.is-black {
+  background-color: black;
+}
+.title {
+  font-weight: bold;
+  font-size: 45px;
+}
+.subtitle {
+  /*font-weight: bold;*/
+  font-size: 25px;
+}
+.author-name {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,51 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-promo-template
-      </h1>
-      <h2 class="subtitle">
-        My super Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+ <Banner />
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Top Video Games</h1>
+        <div class="columns">
+          <div class="column is-one-quarter">
+          <GameCard />
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Featured Articles</h1>
+        <div class="columns">
+          <div class="column is-one-quarter">
+         <ArticleCard />
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+
+import GameCard  from '~/components/shared/GameCard'
+import ArticleCard from '~/components/shared/ArticleCard'
+import Banner from '~/components/shared/Banner'
 
 export default {
   components: {
-    Logo
+    Banner,
+    GameCard,
+    ArticleCard
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style scoped lang="scss">
+  // card item
+  
+  // card item end
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  // Home page
+  .links {
+    padding-top: 15px;
+  }
 </style>
